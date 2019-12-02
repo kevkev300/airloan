db434868eae2ab42fe2ad8c12c7616b8bf2a0503
Kevin Liebholz <kevin@liebholz.eu>
2019-12-02 18:03:03 +0100

    Feat: creating Offer model and database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_164355) do
+ActiveRecord::Schema.define(version: 2019_12_02_165358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offers", force: :cascade do |t|
+    t.string "currency"
+    t.integer "amount"
+    t.integer "interes"
+    t.text "description"
+    t.text "bank_account"
+    t.text "address"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +42,5 @@ ActiveRecord::Schema.define(version: 2019_12_02_164355) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "offers", "users"
 end
