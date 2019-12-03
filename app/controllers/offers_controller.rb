8fbbb412941fe6621b79f8a1e2eae908a43210cb
Kevin Liebholz <kevin@liebholz.eu>
2019-12-03 15:56:33 +0100

    FEAT: wrote the offers#create ---> need to change 'user_id: 1' to current_user once login is up

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -7,5 +7,17 @@ class OffersController < ApplicationController
   end
 
   def create
+    @offer = Offer.new(offer_params.merge({ user_id: 1 }))
+    if @offer.save
+      redirect_to offers_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:amount, :interes, :description, :bank_account, :address)
   end
 end
