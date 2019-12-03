cfa28ce615dc1402b6cf8df15d8eb8ffa2e2a8ab
Angus Tatchell <angustatchell@gmail.com>
2019-12-03 16:21:24 +0100

    user_seeding file not able to produce valid instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@names = ["Norberto", "Jules", "Joseph", "Tyrone", "Edmund", "Minh", "Ervin", "Genaro", "Lindsey", "Cruz", "Zackary", "Carson", "Jason", "Alden", "Stan", "Wilburn", "Cyrus", "Andre", "Rashad", "Noah", "Tim", "Bret", "Donn", "Gregorio", "Hassan", "Jay", "Jeff", "Cletus", "Jon", "Monty", "Foster", "Curtis", "Earle", "Quentin", "Franklin", "Carlo", "Rudolf", "Mathew", "Roosevelt", "Antione", "Claud", "Jacques", "Whitney", "Colby", "Gerardo", "Williams", "Sebastian", "Rueben", "Maynard", "Ben"]
+
+def seed_users
+  User.destroy_all
+  p "Destroying users"
+  p "Seeding users"
+  1000.times do
+    new = User.create(email: "#{@names[rand(50)]}@gmail.com", encrypted_password: a1b2c3d4e5f6, first_name: "#{@names[rand(50)]}", last_name: "#{@names[rand(50)]}")
+    p "#{new.valid?}"
+  end
+  p "Seeded #{User.count} users"
+end
+
+seed_users
+
