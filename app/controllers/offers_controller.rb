456858d89d69ca13d1513363b6753419f9849bc4
Kevin Liebholz <kevin@liebholz.eu>
2019-12-03 16:04:13 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -7,6 +7,7 @@ class OffersController < ApplicationController
   end
 
   def create
+    # !!!!!need to change 'user_id: 1' to current_user once login is up!!!!!
     @offer = Offer.new(offer_params.merge({ user_id: 1 }))
     if @offer.save
       redirect_to offers_path
