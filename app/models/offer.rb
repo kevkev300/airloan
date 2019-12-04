93d348904f86856d7a1626a29bb8917d73100cdc
Kevin Liebholz <kevin@liebholz.eu>
2019-12-04 16:02:39 +0100

    FIX: validate amount not to be negative & WIP link in cards

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,7 +4,7 @@ class Offer < ApplicationRecord
 
   # validations
   validates :currency, presence: true, allow_blank: false
-  validates :amount, presence: true, allow_blank: false
+  validates :amount, presence: true, allow_blank: false, numericality: { greater_than: 0 }
   validates :interest, presence: true, allow_blank: false, inclusion: { in: (0..100) }
   validates :description, presence: true, allow_blank: false
   validates :bank_account, presence: true, allow_blank: false
