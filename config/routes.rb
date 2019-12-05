0853a07c0aa8815b146a47c297ab68e232f1afda
Kevin Liebholz <kevin@liebholz.eu>
2019-12-05 16:30:04 +0100

    FEAT: creating routes and links for accepting and declining a loan request! exit

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :loans, only: [:index]
   resources :users, only: [:show]
+
+  resources :loans, only: [:index] do
+    member do
+        get "/AcceptedByLender", to: "loans#accepted", as: :accepted_by_lender
+        get "/DeclinedByLender", to: "loans#declined", as: :declined_by_lender
+    end
+  end
 
   resources :offers, only: [:index, :new, :create, :show] do
     resources :loans, only: [:new, :create]
