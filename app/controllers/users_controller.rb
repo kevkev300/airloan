da0f9cb87b5cea0be166158e4ef385dbb3123ac2
Kevin Liebholz <kevin@liebholz.eu>
2019-12-07 15:26:41 +0100

    FEAT: colored headlines in dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def show
+    @has_loan_request = has_loan_request
+  end
+
+  private
+
+  def has_loan_request
+    current_user.offers.any? do |offer|
+      offer.loans.count > 0
+    end
   end
 end
